e08f675d11bbb0e79e49a6afd2f1a8fcbfb4c6fc
MakeItEasyDev <89842411+MakeItEasyDev@users.noreply.github.com>
2022-03-18 07:04:56 +0530

    Add files via upload

---
+++ app/src/main/java/com/kotlin/bottomnavigation/ui/setting/SettingViewModel.kt
@@ -0,0 +1,13 @@
+package com.kotlin.bottomnavigation.ui.setting
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SettingViewModel: ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is Setting Fragment"
+    }
+    val text: LiveData<String> = _text
+}
